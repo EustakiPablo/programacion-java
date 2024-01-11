db8e9fd3ba25cf7460e080180edc1bb9d846dff0
CristianFalcon2001 <cristian_fuenla01@hotmail.com>
2024-01-11 14:18:46 +0100

    Segundo commit hoy

---
+++ IdeaProjects/ejercicios/src/Ejercicios/Ejercicio6.java
@@ -0,0 +1,18 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce la distancia a recorrer");
+        double distancia = teclado.nextDouble();
+        System.out.println("Introduce el número de dias de estancia");
+        double estancia = teclado.nextDouble();
+        if (distancia > 1000 && estancia > 7){
+            System.out.println("El precio con descuento es: "+(distancia*8.5-(distancia*8.5*0.3)));
+        }else {
+            System.out.println("El precio es: "+(distancia*8.5));
+        }
+    }
+}
