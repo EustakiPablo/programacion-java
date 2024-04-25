3488ffc411221c983865449289a8ffb003568a04
CristianFalcon2001 <cristian_fuenla01@hotmail.com>
2024-04-25 13:58:10 +0200

    Ejercicios sql

--- SQL/BBDD/tercer trimestre/Procedimientos/Ejercicio. PA que llama a otro PA.sql
+++ SQL/BBDD/tercer trimestre/Procedimientos/Ejercicio. PA que llama a otro PA.sql
@@ -1,58 +1,19 @@
 use empresa3;
 DROP PROCEDURE IF EXISTS minComMult;
-/*SET division = den1/dividir;
-        SET division2 = den2/dividir;
-        IF ((SELECT COUNT(numeros) FROM multiplos WHERE numeros=division) = 0) THEN
-			INSERT INTO multiplos VALUES(division);
-        END IF;
-        IF ((SELECT COUNT(numeros) FROM multiplos WHERE numeros=division2) = 0) THEN
-			INSERT INTO multiplos VALUES(division2);
-        END IF;
-        SET dividir = dividir+1;*/
-    
-    
-    /*WHILE (dividir != den1 AND dividir != den2) DO
-    SET si = den1 % dividir;
-		IF (si != 0) THEN
-			IF ((SELECT numeros FROM multiplos WHERE numeros=si) = null) THEN
-				INSERT INTO multiplos VALUES(si);
-            END IF;
-        END IF;
-        SET dividir = dividir+1;
-    END WHILE;
-    DECLARE dividir INT DEFAULT 2;
-    DECLARE dividir2 INT DEFAULT 2;
-    DECLARE division INT;
-	DECLARE division2 INT;
-    SET division = den1;
-	SET division2 = den2;
-	DROP TABLE IF EXISTS multiplos;
-	CREATE TABLE multiplos(numeros INT);
-    WHILE (division != 1) DO #OR division2 != 1
-		IF (division%dividir!=0) THEN
-			IF ((SELECT COUNT(numeros) FROM multiplos WHERE numeros=dividir) = 0) THEN
-				INSERT INTO multiplos VALUES(dividir);
-                SET division = division%dividir;
-                SET dividir = 2;
-			END IF;
-        END IF;
-		SET dividir = dividir+1;
-		SET dividir2 = dividir2+1;
-    END WHILE;
-    SELECT * FROM multiplos;
-    */
+
 DELIMITER //
 CREATE PROCEDURE minComMult(INOUT den1 INT, INOUT den2 INT)
 BEGIN
 	DECLARE dividir INT DEFAULT 2;
     DECLARE dividir2 INT DEFAULT 2;
+    
     DECLARE division INT;
 	DECLARE division2 INT;
     SET division = den1;
 	SET division2 = den2;
 	DROP TABLE IF EXISTS multiplos;
 	CREATE TABLE multiplos(numeros INT);
-    WHILE (division!=1) DO 
+    WHILE (division!=1 AND division2!=1) DO 
 		IF (division%dividir=0) THEN
 			IF ((SELECT COUNT(numeros) FROM multiplos WHERE numeros=dividir) = 0) THEN
 				INSERT INTO multiplos VALUES(dividir);
@@ -60,24 +21,22 @@ BEGIN
                 SET dividir = 1;
 			END IF;
         END IF;
-		SET dividir = dividir+1;
-    END WHILE;
-    /*SELECT * FROM multiplos;
-    SELECT division2,dividir2;*/
-    WHILE (division2!=1) DO 
-		IF (division2%dividir2=0) THEN
+        IF (division2%dividir2=0) THEN
 			IF ((SELECT COUNT(numeros) FROM multiplos WHERE numeros=dividir2) = 0) THEN
 				INSERT INTO multiplos VALUES(dividir2);
                 SET division2 = division2/dividir2;
                 SET dividir2 = 1;
 			END IF;
         END IF;
-		SET dividir2 = dividir2+1;
+		SET dividir = dividir+1;
+        SET dividir2 = dividir2+1;
     END WHILE;
+      
+
+
     
     
-    
-    SELECT * FROM multiplos;
+    -- SELECT * FROM multiplos;
 END//
 DELIMITER ;
 SET @hola1 = 14;
